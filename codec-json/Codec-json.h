6dc1f2d3a7de16ed229d3fc1293d772d6e44a09d
Casey Marshall <casey.marshall@memeo-inc.com>
2012-02-28 17:09:10 -0800

    codec-json.
    Direct API naming wrapper around jansson, because it looks good enough.

---
+++ codec-json/Codec-json.h
@@ -0,0 +1,14 @@
+//
+//  Codec-json.h
+//  LoungeAct
+//
+//  Created by Casey Marshall on 2/28/12.
+//  Copyright (c) 2012 Memeo, Inc. All rights reserved.
+//
+
+#ifndef LoungeAct_Codec_json_h
+#define LoungeAct_Codec_json_h
+
+
+
+#endif
