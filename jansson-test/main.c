e249946ee0e0d905b09db5313e90adc34850ad83
Casey Marshall <casey.marshall@memeo-inc.com>
2012-03-07 18:45:51 -0800

    Test programs.

--- jansson-test/main.c
+++ jansson-test/main.c
@@ -7,12 +7,30 @@
 //
 
 #include <CoreFoundation/CoreFoundation.h>
+#include <jansson.h>
 
 int main (int argc, const char * argv[])
 {
-
-    // insert code here...
-    CFShow(CFSTR("Hello, World!\n"));
-    return 0;
+    json_error_t error;
+    const char *json = "{\"_rev\":\"2-13839535feb250d3d8290998b8af17c3\",\"foo\":\"bar\",\"baz\":\"baz\",\"quux\":1234}";
+    json_t *obj = json_loads(json, 0, &error);
+    if (obj == NULL)
+    {
+        printf("failed to parse: %d %d %s\n", error.line, error.column, error.text);
+        return 1;
+    }
+    if (!json_is_object(obj))
+    {
+        printf("did not get an object back\n");
+        return 1;
+    }
+    
+    void *iter = json_object_iter(obj);
+    while (iter)
+    {
+        const char *key = json_object_iter_key(iter);
+        printf("object key %s\n", key);
+        iter = json_object_iter_next(obj, iter);
+    }
 }
 
--- revgen-couch-test/main.c
+++ revgen-couch-test/main.c
@@ -7,12 +7,20 @@
 //
 
 #include <CoreFoundation/CoreFoundation.h>
+#include "../revgen/revgen.h"
+#include "../Codec/Codec.h"
 
 int main (int argc, const char * argv[])
 {
-
-    // insert code here...
-    CFShow(CFSTR("Hello, World!\n"));
+    char rev[16];
+    la_codec_value_t *value = la_codec_object();
+    la_revgen(value, 0, NULL, 0, 0, rev, sizeof(rev));
+    const char test_empty_rev[] = { 150,122,0,223,245,224,42,221,65,129,145,56,171,179,40,77 };
+    if (memcmp(rev, test_empty_rev, 16) != 0)
+        printf("FAIL empty new first object");
+    la_codec_decref(value);
+    
+    printf("OK");
     return 0;
 }
 
