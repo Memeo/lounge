d5c6b217950b70fba5a52b6bfa92b36389bb6b51
Casey Marshall <casey.marshall@memeo-inc.com>
2012-03-16 17:13:57 -0700

    New test program.

---
+++ test-pull-bdb-json/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test-pull-bdb-json
+//
+//  Created by Casey Marshall on 3/16/12.
+//  Copyright (c) 2012 Memeo, Inc. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
