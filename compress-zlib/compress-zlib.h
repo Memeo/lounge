d10c74acaa8f0ece082554510a607daf2bbf71c3
Casey Marshall <casey.marshall@memeo-inc.com>
2012-04-02 12:08:17 -0700

    More pluggable compression.

---
+++ compress-zlib/compress-zlib.h
@@ -0,0 +1,14 @@
+//
+//  compress-zlib.h
+//  LoungeAct
+//
+//  Created by Casey Marshall on 4/2/12.
+//  Copyright (c) 2012 Memeo, Inc. All rights reserved.
+//
+
+#ifndef LoungeAct_compress_zlib_h
+#define LoungeAct_compress_zlib_h
+
+
+
+#endif
