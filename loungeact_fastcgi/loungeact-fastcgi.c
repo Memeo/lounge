39a969332d1a18de59655e2ba48b37029254842d
Casey Marshall <casey.marshall@memeo-inc.com>
2012-04-06 17:13:04 -0700

    Fastcgi and nginx stuff.

---
+++ loungeact_fastcgi/loungeact-fastcgi.c
@@ -0,0 +1,9 @@
+//
+//  loungeact-fastcgi.c
+//  LoungeAct
+//
+//  Created by Casey Marshall on 4/2/12.
+//  Copyright (c) 2012 Memeo, Inc. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ ngx_http_loungeact.c
@@ -0,0 +1,9 @@
+//
+//  ngx_http_loungeact.c
+//  LoungeAct
+//
+//  Created by Casey Marshall on 4/2/12.
+//  Copyright (c) 2012 Memeo, Inc. All rights reserved.
+//
+
+#include <stdio.h>
