1c0938e1a8b4396c421d6edb60fb54ababcfb349
Casey Marshall <casey.marshall@memeo-inc.com>
2012-04-27 11:00:28 -0700

    MIT license.

---
+++ js-mapreduce.c
@@ -0,0 +1,9 @@
+//
+//  js-mapreduce.c
+//  LoungeAct
+//
+//  Created by Casey Marshall on 4/11/12.
+//  Copyright (c) 2012 Memeo, Inc. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ js-mapreduce.h
@@ -0,0 +1,14 @@
+//
+//  js-mapreduce.h
+//  LoungeAct
+//
+//  Created by Casey Marshall on 4/11/12.
+//  Copyright (c) 2012 Memeo, Inc. All rights reserved.
+//
+
+#ifndef LoungeAct_js_mapreduce_h
+#define LoungeAct_js_mapreduce_h
+
+
+
+#endif
